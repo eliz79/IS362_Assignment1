92f019b39b2ff2c4171f00f9746e13ff790fd711
Erica Liz <erica.liz@spsmail.cuny.edu>
2017-09-03 11:17:50 -0400

    Second Commit
    Removed file

--- Tabeau Comparison.sql
+++
@@ -1,8 +0,0 @@
-# What airport had the highest departure delay in 2013 grouped by aircraft manufacturer. 
-SELECT origin AS 'Airport', flights.year, flights.dep_delay, planes.manufacturer
-FROM flights
-INNER JOIN planes
-ON flights.tailnum = planes.tailnum
-WHERE planes.year = 2009
-ORDER BY dep_delay desc limit 20;
-
